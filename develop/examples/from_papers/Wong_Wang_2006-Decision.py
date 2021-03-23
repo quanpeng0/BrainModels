323dad34d91c102f11737cc4ff7fb61b417fa4f5
yqianjiang <y.jiang.psy@gmail.com>
2021-03-23 16:10:45 +0800

    update codes to brainpy 1.0.0-alpha

--- develop/examples/from_papers/Wong_Wang_2006-Decision.py
+++ develop/examples/from_papers/Wong_Wang_2006-Decision.py
@@ -15,63 +15,87 @@ We adopt a simplified version from
 
 import brainpy as bp
 import numpy as np
-import matplotlib.pyplot as plt
-
+from numba import prange
 from collections import OrderedDict
 
+bp.backend.set(backend='numba', dt=0.05)
 
-def decision_model(tau_s=.06, gamma=0.641,
+class Decision(bp.NeuGroup):
+    target_backend = 'general'
+
+    def __init__(self, size, coh, tau_s=.06, gamma=0.641,
                 J_rec = .3725, J_inh = .1137, 
-                I_0=.3297,
-                a = 270., b=108., d=0.154):
+                I_0=.3297, JAext = .00117,
+                a = 270., b=108., d=0.154,
+                **kwargs):
+        # parameters
+        self.coh = coh
+        self.tau_s = tau_s
+        self.gamma = gamma
+        self.J_rec = J_rec
+        self.J_inh = J_inh
+        self.I0 = I_0
+        self.JAext = JAext
+        self.a = a
+        self.b = b
+        self.d = d
 
-    ST = bp.types.NeuState('r1', 'r2','I1','I2', s1=0.06, s2=0.06)
+        # variables
+        self.s1 = bp.backend.ones(size) * .06
+        self.s2 = bp.backend.ones(size) * .06
+        self.input = bp.backend.zeros(size)
+
+        super(Decision, self).__init__(size=size, **kwargs)
+
+
+    @staticmethod
+    @bp.odeint(method='rk4')
+    def integral(s1, s2, t, I, coh, JAext, J_rec, J_inh, I_0, b, d, a, tau_s, gamma): 
+        I1 = JAext * I * (1. + coh) 
+        I2 = JAext * I * (1. - coh) 
+
+        I_syn1 = J_rec * s1 - J_inh * s2 + I_0 + I1
+        r1 = (a * I_syn1 - b) / (1. - bp.backend.exp(-d * (a * I_syn1 - b)))
+        ds1dt = - s1 / tau_s + (1. - s1) * gamma * r1
+        
+        I_syn2 = J_rec * s2 - J_inh * s1 + I_0 + I2
+        r2 = (a * I_syn2 - b) / (1. - bp.backend.exp(-d * (a * I_syn2 - b)))
+        ds2dt = - s2 / tau_s + (1. - s2) * gamma * r2        
+
+        return ds1dt, ds2dt
+
+
+    def update(self, _t):
+        for i in prange(self.size):
+            self.s1[i], self.s2[i] = self.integral(self.s1[i], self.s2[i], _t, 
+                                            self.input[i], self.coh, self.JAext, self.J_rec, 
+                                            self.J_inh, self.I0, self.b, self.d, 
+                                            self.a, self.tau_s, self.gamma)
+            self.input[i] = 0.
+
+
+if __name__ == "__main__":
+    # phase plane analysis
+    pars = dict(tau_s=.06, gamma=0.641,
+                J_rec = .3725, J_inh = .1137, 
+                I_0=.3297, JAext = .00117,
+                b=108., d=0.154, a = 270.)
+
+    # pars['I'] = 0.
+    pars['I'] = 30.
+
+    # pars['coh'] = 0.
+    # pars['coh'] = .14
+    pars['coh'] = .512
+    # pars['coh'] = 1.
+
+    phase = bp.analysis.PhasePlane(Decision.integral,
+                                   target_vars=OrderedDict(s2=[0., 1.], s1=[0., 1.]),
+                                   fixed_vars=None,
+                                   pars_update=pars,
+                                   numerical_resolution=.001,
+                                   options={'escape_sympy_solver': True})
     
-    @bp.integrate
-    def int_s1(s1, t, s2, I1): 
-        I_syn = J_rec * s1 - J_inh * s2 + I_0 + I1
-        r1 = (a * I_syn - b) / (1. - np.exp(-d * (a * I_syn - b)))
-        dsdt = - s1 / tau_s + (1. - s1) * gamma * r1
-        return (dsdt,), r1
-
-    @bp.integrate
-    def int_s2(s2, t, s1, I2):
-        I_syn = J_rec * s2 - J_inh * s1 + I_0 + I2
-        r2 = (a * I_syn - b) / (1. - np.exp(-d * (a * I_syn - b)))
-        dsdt = - s2 / tau_s + (1. - s2) * gamma * r2
-        return (dsdt,), r2
-
-    def update(ST, _t):
-        s1, ST['r1'] = int_s1(ST['s1'], _t, ST['s2'],ST['I1'])
-        s2, ST['r2'] = int_s2(ST['s2'], _t, ST['s1'],ST['I2'])
-        ST['s1'] = s1
-        ST['s2'] = s2
-        ST['I1'], ST['I2'] = 0, 0
-
-    return bp.NeuType(name='neuron', 
-                    ST=ST, 
-                    steps=update, 
-                    mode='scalar')
-
-
-def stimulus(coh, mu0=30., JAext=.00117):
-    I1 = JAext * mu0 * (1 + coh) 
-    I2 = JAext * mu0 * (1 - coh) 
-    return I1, I2
-
-
-I1, I2 = stimulus(coh=.512)
-
-analyzer = bp.analysis.PhasePlane(
-    model=decision_model(),
-    target_vars=OrderedDict(s2=[0., 1.], s1=[0., 1.]),
-    fixed_vars={'I1': I1, 'I2':I2},
-    numerical_resolution=0.001,
-    options={'escape_sympy_solver': True}
-)
-
-analyzer.plot_vector_field()
-analyzer.plot_nullcline(numerical_setting={
-                        'int_s1':dict(coords='s1-s2'), 
-                        'int_s2':dict(coords='s2-s1')})
-analyzer.plot_fixed_point(show=True)
+    phase.plot_nullcline()
+    phase.plot_fixed_point()
+    phase.plot_vector_field(show=True)
