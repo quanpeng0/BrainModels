224d854d3e28d31e6764f78e75e59c8df0beee19
yqianjiang <y.jiang.psy@gmail.com>
2021-03-23 14:59:22 +0800

    update CANN to brainpy 1.0.0-alpha

--- develop/examples/networks/CANN_1D.py
+++ develop/examples/networks/CANN_1D.py
@@ -22,149 +22,131 @@ I_{ext} = A\exp\left[-\frac{|x-z(t)|^2}{4a^2}\right]
 import brainpy as bp
 import numpy as np
 
-bp.profile.set(numerical_method='rk4', dt=0.05)
+bp.backend.set(backend='numpy', dt=0.05)
 
-N = 256  # The number of neurons
-tau = 1.  # The synaptic time constant
-k = 8.1  # Degree of the rescaled inhibition
-a = 0.5  # Half-width of the range of excitatory connections
-A = 10.  # Magnitude of the external input
-J0 = 4. / (N / 128)
-z_min = -np.pi
-z_max = np.pi
-z_range = z_max - z_min
-rho = N / z_range  # The neural density
-dx = z_range / N
+class CANN(bp.NeuGroup):
+    target_backend = 'general'
 
+    def __init__(self, size, rho, dx, tau=1., k=8.1,
+                 **kwargs):
+        # parameters
+        self.rho = rho
+        self.dx = dx
+        self.tau = tau
+        self.k = k  # Degree of the rescaled inhibition
 
-def dist(d):
-    d = np.remainder(d, z_range)
-    d = np.where(d > 0.5 * z_range, d - z_range, d)
-    return d
+        # variables
+        self.x = bp.backend.zeros(size)
+        self.u = bp.backend.zeros(size)
+        self.r = bp.backend.zeros(size)
+        self.input = bp.backend.zeros(size)
+        self.Jxx = bp.backend.zeros((size, size))
 
-
-# neuron #
-# ------ #
+        super(CANN, self).__init__(size=size, **kwargs)
 
 
-@bp.integrate
-def int_u(u, t, Jxx, Iext):
-    r1 = np.square(u)
-    r2 = 1.0 + k * rho * np.sum(r1) * dx
-    r = r1 / r2
-    Irec = rho * np.dot(Jxx, r) * dx
-    dudt = (-u + Irec + Iext) / tau
-    return (dudt,), r
+    @staticmethod
+    @bp.odeint(method='rk4')
+    def integral(u, t, Jxx, Iext, k, rho, dx, tau):
+        r1 = np.square(u)
+        r2 = 1.0 + k * rho * bp.backend.sum(r1) * dx
+        r = r1 / r2
+        Irec = rho * np.dot(Jxx, r) * dx
+        dudt = (-u + Irec + Iext) / tau
+        return dudt
 
 
-def neu_update(ST, _t, Jxx):
-    ST['u'], ST['r'] = int_u(ST['u'], _t, Jxx, ST['input'])
-    ST['input'] = 0.
-
-
-requires = {
-    'Jxx': bp.types.Array(dim=2, help='Weight connection matrix.')
-}
-
-cann = bp.NeuType(name='CANN',
-                  steps=neu_update,
-                  ST=bp.types.NeuState(['x', 'u', 'r', 'input']),
-                  requires=requires)
-
+    def update(self, _t):
+        self.u = self.integral(self.u, _t, self.Jxx, self.input, 
+                                        self.k, self.rho, self.dx, self.tau)
+        self.input[:] = 0
 
 # connection #
 # ---------- #
 
+def dist(d, z_range):
+    d = np.remainder(d, z_range)
+    d = np.where(d > 0.5 * z_range, d - z_range, d)
+    return d
 
-def make_conn(x):
+def make_conn(x, J0, z_range):
     assert np.ndim(x) == 1
     x_left = np.reshape(x, (len(x), 1))
     x_right = np.repeat(x.reshape((1, -1)), len(x), axis=0)
-    d = dist(x_left - x_right)
+    d = dist(x_left - x_right, z_range)
     jxx = J0 * np.exp(-0.5 * np.square(d / a)) / (np.sqrt(2 * np.pi) * a)
     return jxx
 
+if __name__ == "__main__":
+    N = 256.
+    
+    a = 0.5  # Half-width of the range of excitatory connections
+    A = 10.   # Magnitude of the external input
+    
+    J0 = 4. / (N / 128)
+    z_min = -np.pi
+    z_max = np.pi
+    z_range = z_max - z_min
+    rho = N / z_range  # The neural density
+    dx = z_range / N
 
-# network #
-# ------- #
+    group = CANN(size=int(N), rho=rho, dx=dx, monitors=['u'])
+    group.x = np.linspace(z_min, z_max, int(N))
+    group.Jxx = make_conn(group.x, J0, z_range)
 
-def population_coding():
-    global k
-    k = 0.1
-    group = bp.NeuGroup(cann, geometry=N, monitors=['u'])
-    group.ST['x'] = np.linspace(z_min, z_max, group.num)
-    group.Jxx = make_conn(group.ST['x'])
 
-    I1 = A * np.exp(-0.25 * np.square(dist(group.ST['x'] - 0.) / a))
-    Iext, duration = bp.inputs.constant_current([(0., 5.), (I1, 30.), (0., 30.)])
-    group.run(duration=duration, inputs=('ST.input', Iext))
+    # population coding
+    I1 = A * np.exp(-0.25 * np.square(dist(group.x - 0., z_range) / a))
+    Iext, duration = bp.inputs.constant_current([(0., 1.), (I1, 8.), (0., 8.)])
+    group.run(duration=duration, inputs=('input', Iext))
 
     bp.visualize.animate_1D(
-        dynamical_vars=[{'ys': group.mon.u, 'xs': group.ST['x'], 'legend': 'u'},
-                        {'ys': Iext, 'xs': group.ST['x'], 'legend': 'Iext'}],
+        dynamical_vars=[{'ys': group.mon.u, 'xs': group.x, 'legend': 'u'},
+                        {'ys': Iext, 'xs': group.x, 'legend': 'Iext'}],
+        show=True,
+        frame_step=1,
+        frame_delay=100,
+        # save_path='encoding.gif'
+    )
+
+    # template matching
+    dur1, dur2, dur3 = 10., 30., 0.
+    num1 = int(dur1 / bp.backend._dt)
+    num2 = int(dur2 / bp.backend._dt)
+    num3 = int(dur3 / bp.backend._dt)
+    Iext = np.zeros((num1 + num2 + num3, group.size[0]))
+    Iext[:num1] = A * np.exp(-0.25 * np.square(dist(group.x + 0.5, z_range) / a))
+    Iext[num1:num1 + num2] = A * np.exp(-0.25 * np.square(dist(group.x - 0., z_range) / a))
+    Iext[num1:num1 + num2] += 0.1 * A * np.random.randn(num2, group.size[0])
+    group.run(duration=dur1 + dur2 + dur3, inputs=('input', Iext))
+
+    bp.visualize.animate_1D(
+        dynamical_vars=[{'ys': group.mon.u, 'xs': group.x, 'legend': 'u'},
+                        {'ys': Iext, 'xs': group.x, 'legend': 'Iext'}],
         show=True,
         frame_step=5,
         frame_delay=50,
-        # save_path='encoding.mp4'
+        # save_path='decoding.gif'
     )
 
-
-def template_matching():
-    global k
-    k = 8.1
-
-    group = bp.NeuGroup(cann, geometry=N, monitors=['u'])
-    group.ST['x'] = np.linspace(z_min, z_max, group.num)
-    group.Jxx = make_conn(group.ST['x'])
-
-    dur1, dur2, dur3 = 20., 50., 0.
-    num1 = int(dur1 / bp.profile.get_dt())
-    num2 = int(dur2 / bp.profile.get_dt())
-    num3 = int(dur3 / bp.profile.get_dt())
-    Iext = np.zeros((num1 + num2 + num3, group.num))
-    Iext[:num1] = A * np.exp(-0.25 * np.square(dist(group.ST['x'] + 0.5) / a))
-    Iext[num1:num1 + num2] = A * np.exp(-0.25 * np.square(dist(group.ST['x'] - 0.) / a))
-    Iext[num1:num1 + num2] += 0.1 * A * np.random.randn(num2, group.num)
-    group.run(duration=dur1 + dur2 + dur3, inputs=('ST.input', Iext))
-
-    bp.visualize.animate_1D(
-        dynamical_vars=[{'ys': group.mon.u, 'xs': group.ST['x'], 'legend': 'u'},
-                        {'ys': Iext, 'xs': group.ST['x'], 'legend': 'Iext'}],
-        show=True,
-        frame_step=5,
-        frame_delay=50,
-        # save_path='decoding.mp4'
-    )
-
-
-def smooth_tracking():
-    group = bp.NeuGroup(cann, geometry=N, monitors=['u'])
-    group.ST['x'] = np.linspace(z_min, z_max, group.num)
-    group.Jxx = make_conn(group.ST['x'])
-
+    # smooth tracking
     dur1, dur2, dur3 = 20., 20., 20.
-    num1 = int(dur1 / bp.profile.get_dt())
-    num2 = int(dur2 / bp.profile.get_dt())
-    num3 = int(dur3 / bp.profile.get_dt())
+    num1 = int(dur1 / bp.backend._dt)
+    num2 = int(dur2 / bp.backend._dt)
+    num3 = int(dur3 / bp.backend._dt)
     position = np.zeros(num1 + num2 + num3)
     position[num1: num1 + num2] = np.linspace(0., 12., num2)
     position[num1 + num2:] = 12.
     position = position.reshape((-1, 1))
-    Iext = A * np.exp(-0.25 * np.square(dist(group.ST['x'] - position) / a))
-    group.run(duration=dur1 + dur2 + dur3, inputs=('ST.input', Iext))
+    Iext = A * np.exp(-0.25 * np.square(dist(group.x - position, z_range) / a))
+    group.run(duration=dur1 + dur2 + dur3, inputs=('input', Iext))
 
     bp.visualize.animate_1D(
-        dynamical_vars=[{'ys': group.mon.u, 'xs': group.ST['x'], 'legend': 'u'},
-                        {'ys': Iext, 'xs': group.ST['x'], 'legend': 'Iext'}],
+        dynamical_vars=[{'ys': group.mon.u, 'xs': group.x, 'legend': 'u'},
+                        {'ys': Iext, 'xs': group.x, 'legend': 'Iext'}],
         show=True,
         frame_step=5,
-        frame_delay=25,
-        # save_path='tracking.mp4'
+        frame_delay=50,
+        # save_path='tracking.gif'
     )
 
-
-if __name__ == '__main__':
-    pass
-    population_coding()
-    # template_matching()
-    # smooth_tracking()
