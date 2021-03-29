1dc29fb7ac99f3b9e48ce4462bdbbbac0286d777
adaliuBC <adaliu1998@163.com>
2021-03-29 10:45:42 +0800

    Update LIF & HR code

--- develop/examples/neurons/HindmarshRose_model.py
+++ develop/examples/neurons/HindmarshRose_model.py
@@ -1,42 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import brainpy as bp
-import numpy as np
 import brainmodels
-import matplotlib.pyplot as plt
 
-# set global params
-dt = 0.02  # update variables per <dt> ms
-bp.profile.set(jit=True, dt=dt, merge_steps=True)
 mode = 'irregular_bursting'
 param= {'quiescence':         [1.0, 2.0],  #a
         'spiking':            [3.5, 5.0],  #c
         'bursting':           [2.5, 3.0],  #d
         'irregular_spiking':  [2.95, 3.3], #h
         'irregular_bursting': [2.8, 3.7],  #g
-        }  #params of b and I_ext corresponding to different firing mode
+        }  
+#set params of b and I_ext corresponding to different firing mode
 print(f"parameters is set to firing mode <{mode}>")
 
-# define neuron type
-HindmarshRose_neuron = brainmodels.neurons.get_HindmarshRose(noise = 0.)
+group = brainmodels.neurons.HindmarshRose(size = 10, b = param[mode][0],
+                      monitors=['V'])
 
-# build neuron group
-neu = bp.NeuGroup(HindmarshRose_neuron, geometry=(10,), monitors=['V', 'y', 'z'])
-neu.pars['b'] = param[mode][0]
+group.run(100., inputs=('input', param[mode][1]), report=True)
+bp.visualize.line_plot(group.mon.ts, group.mon.V, show=True)
 
-# create input
-current, pos_dur = bp.inputs.constant_current([(param[mode][1], 1000.)])
-
-# simulate
-neu.run(duration=pos_dur, inputs=["ST.input", current], report=True)
-
-# paint
-ts = neu.mon.ts
-fig, gs = bp.visualize.get_figure(1, 1, 4, 8)
-fig.add_subplot(gs[0, 0])
-plt.plot(ts, neu.mon.V[:, 0], label = "V")
-plt.xlabel('Time (ms)')
-plt.ylabel('Membrane potential')
-plt.xlim(-0.1, ts[-1] + 0.1)
-plt.legend()
-plt.show()
+# phase plane analysis
+phase = bp.analysis.PhasePlane(
+    brainmodels.neurons.HindmarshRose.integral,
+    target_vars={'V': [-3, 3], 'y': [-20., 5.]},
+    fixed_vars={'z': 0.},
+    pars_update={'I_ext': param[mode][1], 
+                 'a': 1., 'b': 3., 'c': 1., 'd': 5., 
+                 'r': 0.01, 's': 4., 'V_rest': -1.6})
+phase.plot_nullcline()
+phase.plot_fixed_point()
+phase.plot_vector_field()
+phase.plot_trajectory([{'V': 1., 'y': 0., 'z':-0.0}],
+                      duration=100.,
+                      show=True)
