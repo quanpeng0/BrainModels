3955a39a29d7c77aab72bb04a2d384cb72e9d48c
adaliuBC <adaliu1998@163.com>
2021-04-07 15:23:46 +0800

    Fix the bug of LIF refractory period.

--- develop/examples/neurons/LIF_model.py
+++ develop/examples/neurons/LIF_model.py
@@ -2,12 +2,13 @@
 import brainpy as bp
 import brainmodels
 
-dt = 0.02
+dt = 0.1
 bp.backend.set('numpy', dt=dt)
 neu = brainmodels.neurons.LIF(100, monitors=['V', 'refractory', 'spike'])
-
+neu.t_refractory = 5.
 net = bp.Network(neu)
 net.run(duration=200., inputs=(neu, 'input', 21.), report=True)
+fig, gs = bp.visualize.get_figure(3, 1, 4, 10)
+fig.add_subplot(gs[0, 0])
 bp.visualize.line_plot(neu.mon.ts, neu.mon.V,
-                       xlabel="t", ylabel="V",
-                       show=True)
+                       xlabel="t", ylabel="V")
