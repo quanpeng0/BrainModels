8e3027f8c0c20eb3e2fa80cc0eb7450eec9a5636
adaliuBC <adaliu1998@163.com>
2021-04-23 13:16:06 +0800

    Update figs.

--- develop/examples/neurons/HH_model.py
+++ develop/examples/neurons/HH_model.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
-
-
 import brainpy as bp
 import brainmodels
 
 backend = 'numba'
 bp.backend.set(backend=backend, dt=.01)
 brainmodels.set_backend(backend=backend)
+neu = brainmodels.neurons.HH(100, monitors=['V'])
+net = bp.Network(neu)
+net.run(200., inputs=('input', 10.), report=True)
 
-group = brainmodels.neurons.HH(100, monitors=['V'])
-
-group.run(200., inputs=('input', 10.), report=True)
-bp.visualize.line_plot(group.mon.ts, group.mon.V, show=True)
-
-group.run(200., report=True)
-bp.visualize.line_plot(group.mon.ts, group.mon.V, show=True)
+bp.visualize.line_plot(neu.mon.ts, neu.mon.V, show=True)
+neu.run(200., report=True)
+bp.visualize.line_plot(neu.mon.ts, neu.mon.V, show=True)
