4f7f33024462272badcceeb40d179cd59220c68d
adaliuBC <adaliu1998@163.com>
2021-04-28 10:57:40 +0800

    Modify show code for ppt

--- docs/brainpy_handbook/ppt/codes/snns/EI_balance_network.py
+++ docs/brainpy_handbook/ppt/codes/snns/EI_balance_network.py
@@ -3,7 +3,6 @@ import brainpy as bp
 import brainmodels
 import matplotlib.pyplot as plt
 import numpy as np
-from numba import prange
 
 bp.backend.set('numba')
 
@@ -16,94 +15,33 @@ V_rest = -52.
 V_reset = -60.
 V_th = -50.
 
-class LIF(bp.NeuGroup):
-    target_backend = 'general'
-
-    def __init__(self, size, V_rest = V_rest, V_reset = V_reset,
-                 V_th = V_th, tau = tau, **kwargs):
-        self.V_rest = V_rest
-        self.V_reset = V_reset
-        self.V_th = V_th
-        self.tau = tau
-
-        self.V = bp.ops.zeros(size)
-        self.input = bp.ops.zeros(size)
-        self.spike = bp.ops.zeros(size, dtype = bool)
-
-        super(LIF, self).__init__(size = size, **kwargs)
-
-    @staticmethod
-    @bp.odeint
-    def integral(V, t, I_ext, V_rest, tau):
-        return (-V + V_rest + I_ext) / tau
-
-    def update(self, _t):
-        V = self.integral(self.V, _t, self.input, self.V_rest, self.tau)
-        sp = V > self.V_th
-        V[sp] = self.V_reset
-        self.V = V
-        self.spike = sp
-        self.input[:] = 0.
-
 tau_decay = 2.
-JE = 1 / np.sqrt(prob * N_E)
-JI = 1 / np.sqrt(prob * N_I)
 
-class Syn(bp.TwoEndConn):
-    target_backend = 'general'
-
-    def __init__(self, pre, post, conn,
-                 tau_decay = tau_decay, w = 0., 
-                 **kwargs):
-        self.tau_decay = tau_decay
-        self.w = w
-
-        self.conn = conn(pre.size, post.size)
-        self.pre_ids, self.post_ids = conn.requires('pre_ids', 'post_ids')
-        self.size = len(self.pre_ids)
-
-        self.s = bp.ops.zeros(self.size)
-        self.g = bp.ops.zeros(self.size)
-
-        super(Syn, self).__init__(pre = pre, post = post, **kwargs)
-    
-    @staticmethod
-    @bp.odeint(method = 'rk4')
-    def integral(s, t, tau):
-        return -s / tau
-
-    def update(self, _t):
-        for i in prange(self.size):
-            self.s[i] = self.integral(self.s[i], _t, self.tau_decay)
-            pre_id = self.pre_ids[i]
-            self.s[i] += self.pre.spike[pre_id]
-            g = self.w * self.s[i]
-            post_id = self.post_ids[i]
-            self.post.input[post_id] += g
-
-neu_E = LIF(N_E, monitors = ['spike'])
-neu_I = LIF(N_I, monitors = ['spike'])
+neu_E = brainmodels.neurons.LIF(N_E, monitors=['spike'])
+neu_I = brainmodels.neurons.LIF(N_I, monitors=['spike'])
 neu_E.V = V_rest + np.random.random(N_E) * (V_th - V_rest)
 neu_I.V = V_rest + np.random.random(N_I) * (V_th - V_rest)
 
-syn_E2E = Syn(pre = neu_E, post = neu_E,
-              conn = bp.connect.FixedProb(prob = prob))
-syn_E2I = Syn(pre = neu_E, post = neu_I,
-              conn = bp.connect.FixedProb(prob = prob))
-syn_I2E = Syn(pre = neu_I, post = neu_E,
-              conn = bp.connect.FixedProb(prob = prob))
-syn_I2I = Syn(pre = neu_I, post = neu_I,
-              conn = bp.connect.FixedProb(prob = prob))
+syn_E2E = brainmodels.synapses.Exponential(pre=neu_E, post=neu_E,
+                                           conn=bp.connect.FixedProb(prob=prob))
+syn_E2I = brainmodels.synapses.Exponential(pre=neu_E, post=neu_I,
+                                           conn=bp.connect.FixedProb(prob=prob))
+syn_I2E = brainmodels.synapses.Exponential(pre=neu_I, post=neu_E,
+                                           conn=bp.connect.FixedProb(prob=prob))
+syn_I2I = brainmodels.synapses.Exponential(pre=neu_I, post=neu_I,
+                                           conn=bp.connect.FixedProb(prob=prob))
+
+JE = 1 / np.sqrt(prob * N_E)
+JI = 1 / np.sqrt(prob * N_I)
 syn_E2E.w = JE
 syn_E2I.w = JE
 syn_I2E.w = -JI
 syn_I2I.w = -JI
 
-net = bp.Network(neu_E, neu_I, 
-                 syn_E2E, syn_E2I, 
+net = bp.Network(neu_E, neu_I,
+                 syn_E2E, syn_E2I,
                  syn_I2E, syn_I2I)
-net.run(500., inputs = [(neu_E, 'input', 3.), (neu_I, 'input', 3.)], report = True)
-
+net.run(500., inputs=[(neu_E, 'input', 3.), (neu_I, 'input', 3.)], report=True)
 
 fig, gs = bp.visualize.get_figure(4, 1, 2, 10)
 fig.add_subplot(gs[:3, 0])
